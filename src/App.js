bda8d151c1718431e3ceadeae5233602b44d9126
racray <rayedkhan007@gmail.com>
2021-11-01 17:29:08 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+export default function App(){
+  const users =[{name:"Achyut",pic:"http://www."},{name:"Rayed",pic:"http://www."},{name:"Robin",pic:"http://www."},{name:"Akash",pic:"http://www."}]
+  return(
+    <div className="App">
+ 
+      {users.map((ur)=> <Msg name={ur.name} pic={ur.pic} /> )}
+    </div>
+  )
+}
+function Msg({name,pic}) {
+  return(
+    <div>
+      <img src={pic} alt={name}/>
+      <h1>{name} </h1>
+    </div>
+  )
+
+}
+
+
