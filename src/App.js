6140b2d728a6bd40773d913a5455b68d9c05d0be
racray <rayedkhan007@gmail.com>
2021-11-02 14:51:42 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 export default function App(){
-  const users =[{name:"Achyut",pic:"http://www."},{name:"Rayed",pic:"http://www."},{name:"Robin",pic:"http://www."},{name:"Akash",pic:"http://www."}];
+  // const users =[{name:"Achyut",pic:"http://www."},{name:"Rayed",pic:"http://www."},{name:"Robin",pic:"http://www."},{name:"Akash",pic:"http://www."}];
   const movies = [{title:"Fight Club", picture:"http://cdn.shopify.com/s/files/1/0151/0741/products/PGX0013_1024x1024.jpg?v=1578633303"},{title:"Avengers: End Game", picture:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/d1pklzbuyaab0la-1552597012.jpg"},{title:"The Prestige", picture:"https://alternativemovieposters.com/wp-content/uploads/2021/02/FaronFlood_ThePrestige.jpg"},{title:"Interstellar", picture:"https://i.pinimg.com/originals/26/ee/c3/26eec32582fabc16d00cb64f37f2a393.jpg"},{title:"Avatar", picture:"https://movieposters2.com/images/670908-b.jpg"},{title:"Inception", picture:"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg"}];
   return(
     <div className="App">
@@ -11,14 +11,14 @@ export default function App(){
     </div>
   )
 }
-function Msg({name,pic}) {
-  return(
-    <div>
-      <img src={pic} alt={name}/>
-      <h1>{name} 5 </h1>
-    </div>
-  )
-}
+// function Msg({name,pic}) {
+//   return(
+//     <div>
+//       <img src={pic} alt={name}/>
+//       <h1>{name} 5 </h1>
+//     </div>
+//   )
+// }
 
 function Mvs({title,picture}) {
   return (
